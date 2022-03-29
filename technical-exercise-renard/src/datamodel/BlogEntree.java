7dc1328e3257045d42757032ebeb919552b55c7f
nathan renard <nathanrenard510@gmail.com>
2022-03-29 10:20:57 -0500

    added datamodel

---
+++ technical-exercise-renard/src/datamodel/BlogEntree.java
@@ -0,0 +1,74 @@
+package datamodel;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//Song Name, Artist, Album Name, 
+//a short description of why I like the song, 
+//and a link to youtube to listen to the song.
+
+/**
+*@since J2SE-1.8
+CREATE TABLE JazzBlog(
+	id INT NOT NULL AUTO_INCREMENT,
+	SONG_NAME VARCHAR(50) NOT NULL,
+	ARTIST_NAME VARCHAR(60) NOT NULL,
+	ALBUM_NAME VARCHAR(30) NOT NULL,
+	DESC VARCHAR(1000) NOT NULL,
+	YT_LINK VARCHAR(100) NOT NULL
+	PRIMARY KEY(id));
+*/
+
+@Entity
+@Table(name = "JazzBlog")
+public class BlogEntree {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "SONG_NAME")
+	private String songName;
+	
+	@Column(name = "ARTIST_NAME")
+	private String artistName;
+	
+	@Column(name = "ALBUM_NAME")
+	private String albumName;
+	
+	@Column(name = "DESC")
+	private String desc;
+	
+	@Column(name = "YT_LINK")
+	private String ytLink;
+	
+	public BlogEntree() {
+	}
+	
+	public BlogEntree(Integer id, String songName, String artistName, String albumName, String desc, String ytLink) {
+		this.id = id;
+		this.songName = songName;
+		this.artistName = artistName;
+		this.albumName = albumName;
+		this.desc = desc;
+		this.ytLink = ytLink;
+	}
+	
+	public BlogEntree(String songName, String artistName, String albumName, String desc, String ytLink) {
+		this.songName = songName;
+		this.artistName = artistName;
+		this.albumName = albumName;
+		this.desc = desc;
+		this.ytLink = ytLink;
+	}
+	
+	public BlogEntree(String songName, String artistName, String albumName, String ytLink) {
+		this.songName = songName;
+		this.artistName = artistName;
+		this.albumName = albumName;
+		this.ytLink = ytLink;
+	}
+}
